0b600a3055e0d25f9ab25ee8c00d21f7def86b95
Huu_Tu <huutu7165@gmail.com>
2025-03-07 17:10:27 +0700

    f

--- fptclient/src/components/Layout/Main/index.tsx
+++ fptclient/src/components/Layout/Main/index.tsx
@@ -1,10 +1,21 @@
 import { Layout } from 'antd';
 import { Outlet } from 'react-router-dom';
+import classes from './Style/Main.module.scss';
+import AppHeader from "./AppHeader";
+import AppFooter from "./AppFooter";
 
 const MainLayout = () =>{
     return(
-        <Layout>
-            <Outlet />
+        <Layout className={`h-screen ${classes.dashboard}`}>
+            <Layout.Header>
+                <AppHeader />
+            </Layout.Header>
+            <Layout.Content>
+                <Outlet />
+            </Layout.Content>
+            <Layout.Footer>
+                <AppFooter/>
+            </Layout.Footer>
         </Layout>
     )
 };
